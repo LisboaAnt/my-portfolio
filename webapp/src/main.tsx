5c9bc64911292ec790bd07864df59081694c197b
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-05-16 00:28:15 -0300

    Languages and Routes

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './i18n/index.ts'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home/index.tsx'
+import NoPage from './pages/NoPage/index.tsx'
+import Cursos from './pages/Cursos/index.tsx'
+import ContactDetails from './routes/ContactDetails.tsx'
+import Contact from './routes/Contact.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <NoPage/>,
+    children: [  
+      {
+        path: "/",
+        element: <Home/> 
+    },{
+      path: "/cursos",
+      element: <Cursos/> 
+    },{
+      path: "/contact/:id",
+      element: <ContactDetails/>
+    },{
+      path: "/contact/",
+      element: <Contact/>
+    }
+  ] 
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
