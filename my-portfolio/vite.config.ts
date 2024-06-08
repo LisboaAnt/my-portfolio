40465121da1d39c32401c7732aef7ea23d504c14
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-06-08 00:51:54 -0300

    update

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/', // Substitua 'my-portfolio' pelo nome do seu repositório
   plugins: [react()],
 })
