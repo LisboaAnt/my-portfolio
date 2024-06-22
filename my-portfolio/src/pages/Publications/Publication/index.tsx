3182cbd1730bb2e57b3b98eb6acdacceba2e8ff3
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-06-22 01:34:06 -0300

    fix/remove

--- my-portfolio/src/pages/Publications/Publication/index.tsx
+++ my-portfolio/src/pages/Publications/Publication/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import { Worker, Viewer } from '@react-pdf-viewer/core';
