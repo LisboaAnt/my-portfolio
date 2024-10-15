7571e281594fb246d5d1754a054fdde6dcdb2f89
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-10-14 21:41:33 -0300

    fix links github and linkedin

--- my-portfolio/src/components/Layout/Footer.tsx
+++ my-portfolio/src/components/Layout/Footer.tsx
@@ -24,8 +24,8 @@ const Footer = React.memo(() => {
           <li className="list-inline-item"><Link onClick={handleLogoClick} to="/projects" className='greenH'>{t("NavBar.DownMenu.projects")}</Link></li>
           <li className="list-inline-item"><Link onClick={handleLogoClick} to="/publications" className='yellowH'>{t("NavBar.DownMenu.publications")}</Link></li>
           <li className="list-inline-item redH"><Link onClick={handleLogoClick} to="https://www.youtube.com/@antoniolisboa3897/videos" target="_blank" className='redH'>{t("Youtube")}</Link></li>
-          <li className="list-inline-item"><Link onClick={handleLogoClick} to="https://linkedin.com" target="_blank" className='blueH'>{t("Linkedin")}</Link></li>
-          <li className="list-inline-item"><Link onClick={handleLogoClick} to="https://github.com" target="_blank" className='blackH'>{t("Github")}</Link></li>
+          <li className="list-inline-item"><Link onClick={handleLogoClick} to="https://www.linkedin.com/in/antonio-lisboa-de-carvalho-b5a5a4194/" target="_blank" className='blueH'>{t("Linkedin")}</Link></li>
+          <li className="list-inline-item"><Link onClick={handleLogoClick} to="https://github.com/LisboaAnt" target="_blank" className='blackH'>{t("Github")}</Link></li>
         </ul>
       </div>
       <div className="Footer-section">
